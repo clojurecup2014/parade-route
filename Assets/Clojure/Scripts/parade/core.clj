b728cf7caec9a4240c008736193db1b977c8bded
selfsame <joseph.parker.1@gmail.com>
2014-09-27 15:09:08 -0400

    basic game board and tile data in place.  Scene has nice enough graphics.

--- Assets/Clojure/Scripts/parade/core.clj
+++ Assets/Clojure/Scripts/parade/core.clj
@@ -5,6 +5,9 @@
             ))
  
 
+(in-ns 'user)
+(require 'unity.core)
+(use 'unity.messages 'hard.core 'hard.input)
 
 (unity.core/defcomponent Brain []
 	(Awake [this]
@@ -14,18 +17,50 @@
 	(Start [this]
 		(log "hard core in parade Brain"))
 	(Update [this]
-		)) 
+		(when (mouse-down?) (log "mouse down!")))) 
    
  
- (in-ns 'user)
 
- (let [empty-tile (resource "empty-tile")
- 		game-map (GameObject. "game-map")
- 	   tiles (for [x (range 10)
+(def fabs 
+	{:empty-tile (resource "empty-tile")
+	 :base (resource "base")
+	 :road (resource "road")
+	 :road-curve (resource "road-curve")
+	 :road-intersection (resource "road-intersection")})
+
+(defn make-tile []
+	(let [chosen (rand-nth [:road :road-curve :road-intersection])
+		  base (clone! (:base fabs))
+		  road (clone! (chosen fabs))
+		  sig (chosen {:road [1 0 1 0]
+		  			   :road-curve [0 0 1 1]
+		  			   :road-intersection [1 1 1 1]})
+		  rot (rand-int 4)]
+		  (parent! road base)
+		  {:sig (take 4 (drop rot (cycle sig)))
+		  	:go base}
+		  ))
+
+(declare brain game-map)
+(destroy! [brain game-map])
+(def brain (GameObject. "brain"))
+(.AddComponent brain Brain)
+(def game-map (GameObject. "game-map"))
+
+(def que 
+	(doall (for 
+		[idx (range 5)
+		 :let [t (make-tile)]]
+		 (do
+		 	(position! t [-2 (* idx -1) 5])
+		 	(local-scale! t [2 2 2])
+		 	t))))
+
+ (def tiles (into {} (for [x (range 10)
  	   				z (range 10)
- 	   				:let [t (clone! empty-tile [x 0 z])]]
+ 	   				:let [t (clone! (:empty-tile fabs) [x 0 z])]]
  	   				(do 
  	   					(set! (.name t) (str x "-" z))
- 	   					(parent! t game-map)))]
- 	   tiles)
+ 	   					(parent! t game-map)
+ 	   					{[x z] {:go t :sig [0 0 0 0]}})))])
   
